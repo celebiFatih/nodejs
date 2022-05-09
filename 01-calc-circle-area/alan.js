152cd920669c5ae5ce1d181222bbfcbfd62079bc
fatih celebi <fatihcelebi13@gmail.com>
2022-05-09 18:09:37 +0300

    update file

--- 01-calc-circle-area/alan.js
+++ 01-calc-circle-area/alan.js
@@ -3,7 +3,7 @@ let args = process.argv.slice(2);
 const calcArea = (r) => {
     const PI = Math.PI;
     const result = PI * r**2;
-    console.log(result)
+    console.log(`Yarıçapı ${r} olan dairenin alanı: ${result}`)
 }
 
 calcArea(args[0]*1)

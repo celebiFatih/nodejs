976b876255e5d60e6edfd9b4ceda614cc6ca7817
fatih celebi <fatihcelebi13@gmail.com>
2022-05-11 22:12:58 +0300

    template engine conf

--- 07-clean-blog/app.js
+++ 07-clean-blog/app.js
@@ -1,16 +1,25 @@
 const express = require('express');
 const app = express();
+const ejs = require('ejs');
 const port = 3000;
 
-app.get('/', (req,res) => {
-    const blog = {
-        id: 1,
-        title: "Blog Title",
-        desc : "Blog desc"
-    }
-    res.send(blog);
-})
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+app.get('/add_post', (req, res) => {
+  res.render('add_post');
+});
+app.get('/post', (req, res) => {
+    res.render('post');
+  });
 
 app.listen(port, () => {
-    console.log(`Sunucu ${port} portunda çalıştırılmaya başlandı...`)
-})
+  console.log(`Sunucu ${port} portunda çalıştırılmaya başlandı...`);
+});
